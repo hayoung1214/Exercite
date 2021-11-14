f9e66fe0210f3e30376b67e36dc0bb88ef4b2a15
hayoung <hayoung1214@naver.com>
2021-11-14 17:38:39 +0900

    fix : session 적용

--- login_sql.php
+++ login_sql.php
@@ -1,12 +1,13 @@
 <?php
+    session_start();
     $host='localhost';
     $user='team10';
     $password='team10';
     $dbname='team10';
 
     //변수지정
-    $id=$_POST['id'];
-    $pw=$_POST['password'];
+    $user_id=$_POST['id'];
+    $user_pw=$_POST['password'];
    
 
     $conn=new mysqli($host, $user, $password, $dbname);
@@ -24,45 +25,26 @@
     $num=mysqli_num_rows($result);
     if($num==1){
         $row =  mysqli_fetch_array($result);
-        if($row['password']==$pw){
-            echo '로그인 하였습니다.';
+        if($row['password']==$user_pw){
+            echo " <script> alert('로그인을 성공하였습니다.'); </script>";
+            $_SESSION['user_id'] = $row["id"]; //세션에 로그인 성공한 유저의 아이디와 비밀번호 저장
+            $_SESSION['user_password'] = $row["password"];
+            //echo $_SESSION['user_id'] , $_SESSION['user_password'] ;
+            mysqli_close($conn);
+            echo " <meta http-equiv='refresh' content='0 url=main.php'> "; // 로그인 후 연결되는 페이지 
         }
         else{
-            echo '비밀번호가 맞지 않습니다.';
+            echo " <script> alert('비밀번호가 맞지 않습니다.'); </script>";
+            echo "<meta http-equiv='refresh' content='0 url=login.php'>";
+            exit();
         }
     }
     else{
-        echo '해당 아이디가 존재하지 않습니다.';
+        echo " <script> alert('해당 아이디가 존재하지 않습니다.'); </script>";
+        echo "<meta http-equiv='refresh' content='0 url=login.php'>";
+        exit();
+
     }
-    echo $member;
-    //$stmt->bind_result($userID, $userPassword);
-    
-
-    // $check=0;
-    // $method='"post"';
-    // while($id_row=$stmt->fetch()){
-    //     if($userEmail == $id && $userPassword == $pw){
-    //     $check=1;
-    //     $name="myvar";
-    //     $value=$userUserID;
-    //     setcookie($name,$value);
-    //     $name="myvar2";
-    //     $value=$id;
-    //     setcookie($name,$value);
-    //     $name="myvar10";
-    //     $value= $userUserType;
-    //     setcookie($name,$value);
-    //     Header("Location:./ver2_main.php");
-
-    //     if($userUserType==0){
-    //         Header("Location:./customer_info.php");
-    //     }
-       
-    //     exit;
-    //     }
-    // }
-    // if($check==0) {
-    //     Header("Location:./login_ye1.php");
-    // }
+ 
  
 ?>
