5ec4d29ad9d0500cbde6bfc38bb4179ab3016119
leejejang <yds04312@naver.com>
2021-11-18 10:42:44 +0900

    비밀번호 관련 코드

---
+++ php/sign_out.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+    $host='localhost';
+    $user='team10';
+    $password='team10';
+    $dbname='team10';
+
+    //변수지정
+   
+    $user_id=$_POST['user_id'];
+
+ 
+   
+    $conn=new mysqli($host, $user, $password, $dbname);
+    if($conn){
+        echo 'sucess mysql connect</p>';
+    }
+    else{
+        echo '<p>Error: Could not connect to database.<br/> Please try agin later.</p>';
+        exit;
+    }
+
+    $del_reg="DELETE FROM registeration WHERE registeration.user_id='".$user_id."'";
+    $result = mysqli_query($conn, $del_reg);
+
+    $del_user="DELETE FROM user WHERE user.user_id='".$user_id."'";
+    $result2 = mysqli_query($conn, $del_user);
+
+   
+    if(!$result || !$result2){
+        echo "delete 에러";
+    }
+    else{
+        echo " <script> alert('계정이 삭제되었습니다.'); </script>";
+        // echo "<meta http-equiv='refresh' content='0 url=../php/mypage.php'>";
+        exit();
+
+    }
+ 
+ 
+?>
---
+++ php/update_pw.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+    $host='localhost';
+    $user='team10';
+    $password='team10';
+    $dbname='team10';
+
+    //변수지정
+   
+    $user_id=$_POST['user_id']; //$_session??
+    $user_pw=$_$_POST['re_pw'];
+ 
+   
+    $conn=new mysqli($host, $user, $password, $dbname);
+    if($conn){
+        echo 'sucess mysql connect</p>';
+    }
+    else{
+        echo '<p>Error: Could not connect to database.<br/> Please try agin later.</p>';
+        exit;
+    }
+
+    $res="UPDATE user SET password = '".$user_pw."'
+    WHERE user.id='".$user_id."'";
+    $result = mysqli_query($conn, $res);
+
+
+
+   
+    if(!$result ){
+        echo "update 에러";
+    }
+    else{
+        echo " <script> alert('비밀번호가 변경되었습니다.'); </script>";
+        // echo "<meta http-equiv='refresh' content='0 url=../php/mypage.php'>";
+        exit();
+
+    }
+ 
+ 
+?>
